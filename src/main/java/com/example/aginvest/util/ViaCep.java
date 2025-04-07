7cce80fe49fb2a487b18e52cb5482190dccf0a9e
leonardo.borges <leo.nunes.borges@hotmail.com>
2025-04-07 09:21:00 -0300

    cep funcionando

--- src/main/java/com/example/aginvest/util/ViaCep.java
+++ src/main/java/com/example/aginvest/util/ViaCep.java
@@ -2,6 +2,8 @@ package com.example.aginvest.util;
 
 import com.example.aginvest.model.Endereco;
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -24,7 +26,26 @@ public class ViaCep {
         // Enviando a requisição e capturando a resposta
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
         System.out.println("Resposta da API: " + response.body());
-        // Convertendo JSON para objeto Java
-        return new Gson().fromJson(response.body(), Endereco.class);
+
+
+        if (response.statusCode() == 200) {
+            JsonObject jsonResponse = JsonParser.parseString(response.body()).getAsJsonObject();
+
+            Endereco endereco = new Endereco();
+            endereco.setCep(jsonResponse.get("cep").getAsString());
+            endereco.setLogradouro(jsonResponse.get("logradouro").getAsString());
+            endereco.setComplemento(jsonResponse.has("complemento") ? jsonResponse.get("complemento").getAsString() : null);
+            endereco.setBairro(jsonResponse.get("bairro").getAsString());
+            endereco.setLocalidade(jsonResponse.get("localidade").getAsString());
+            endereco.setUf(jsonResponse.get("uf").getAsString());
+            endereco.setIbge(jsonResponse.get("ibge").getAsString());
+            endereco.setGia(jsonResponse.get("gia").getAsString());
+            endereco.setDdd(jsonResponse.get("ddd").getAsString());
+            endereco.setSiafi(jsonResponse.get("siafi").getAsString());
+
+            return endereco;
+        } else {
+            throw new Exception("Erro ao buscar CEP. Status code: " + response.statusCode());
+        }
     }
 }
